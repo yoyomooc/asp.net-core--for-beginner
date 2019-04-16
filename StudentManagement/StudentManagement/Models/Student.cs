2a91096310778a09093b85819c2ba3224f48818f
角落的白板报 <werltm@hotmail.com>
2019-04-16 16:54:53 +0800

    补充内容。

--- StudentManagement/StudentManagement/Models/Student.cs
+++ StudentManagement/StudentManagement/Models/Student.cs
@@ -10,5 +10,6 @@ namespace StudentManagement.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public string ClassName { get; set; }
+        public string Email { get;  set; }
     }
 }
