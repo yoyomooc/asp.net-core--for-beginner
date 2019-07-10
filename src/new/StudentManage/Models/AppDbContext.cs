6eb49bc7175722958df7ee667a9442af2d90862c
桐铭 梁 <werltm@hotmail.com>
2019-07-10 21:26:15 +0800

    添加DbContext

---
+++ src/new/StudentManage/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+
+        }
+
+
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
