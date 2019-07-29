2ef8efc8dd3070146ac157c5a282dcbb5394349c
桐铭 梁 <werltm@hotmail.com>
2019-07-29 12:44:30 +0800

    完成单图片的上传。

--- StudentManagement/StudentManagement/Controllers/HomeController.cs
+++ StudentManagement/StudentManagement/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
 using StudentManagement.Models;
 using StudentManagement.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace StudentManagement.Controllers
 {
@@ -10,11 +13,13 @@ namespace StudentManagement.Controllers
     public class HomeController : Controller
     {
         private readonly IStudentRepository _studentRepository;
+        private readonly HostingEnvironment hostingEnvironment;
 
         //使用构造函数注入的方式注入IStudentRepository
-        public HomeController(IStudentRepository studentRepository)
+        public HomeController(IStudentRepository studentRepository, HostingEnvironment hostingEnvironment)
         {
             _studentRepository = studentRepository;
+            this.hostingEnvironment = hostingEnvironment;
 
         }
 
@@ -51,13 +56,38 @@ namespace StudentManagement.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Student student)
+        public IActionResult Create(StudentCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
 
-                Student newStudent = _studentRepository.Add(student);
-               // return RedirectToAction("Details", new { id = newStudent.Id });
+                string uniqueFileName = null;
+
+                if (model.Photo!=null)
+                {
+
+    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+
+      uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder,uniqueFileName);
+
+                    model.Photo.CopyTo(new FileStream(filePath,FileMode.Create));
+
+                }
+                //c8a3f5d5-f0ad-4079-b192-5bb6358b3c9a_banner.jpg
+
+
+
+                Student newStudent = new Student
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    ClassName = model.ClassName,
+                    PhotoPath = uniqueFileName
+                };
+
+            _studentRepository.Add(newStudent);
+                return RedirectToAction("Details", new { id = newStudent.Id });
             }
             return View();
         }
