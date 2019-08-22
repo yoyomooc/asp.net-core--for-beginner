74e50068c296d981262bcd8bcc4ceaf33f6b91eb
桐铭 梁 <werltm@hotmail.com>
2019-08-22 16:06:38 +0800

    完成Nlog记录日志到文件中。

--- StudentManagement/StudentManagement/Controllers/ErrorController.cs
+++ StudentManagement/StudentManagement/Controllers/ErrorController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,22 @@ namespace StudentManagement.Controllers
 {
     public class ErrorController:Controller
     {
+        private ILogger<ErrorController> logger;
+
+        /// <summary>
+        /// 通过ASP.Net Core 依赖注入服务注入ILogger服务
+        /// 将指定类型的控制器作为泛型参数
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+
+        }
+
+
+
 
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
@@ -26,8 +43,11 @@ namespace StudentManagement.Controllers
                 case 404:
 
                     ViewBag.ErrorMessage = "抱歉，您访问的页面不存在";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QueryStr = statusCodeResult.OriginalQueryString;
+
+     logger.LogWarning($"发生了一个404错误。路径={statusCodeResult.OriginalPath}以及查询字符串={statusCodeResult.OriginalQueryString}");
+
+                    //ViewBag.Path = statusCodeResult.OriginalPath;
+                    //ViewBag.QueryStr = statusCodeResult.OriginalQueryString;
                     //ViewBag.BasePath = statusCodeResult.OriginalPathBase;
 
                     break;
@@ -43,12 +63,14 @@ namespace StudentManagement.Controllers
         [Route("Error")]
         public IActionResult Error()
         {
-
          var exceptionHandlerPathFeature=   HttpContext.Features.Get<IExceptionHandlerPathFeature>();
 
-            ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
-            ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
-            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+            logger.LogError($"路径:{exceptionHandlerPathFeature.Path},产生了一个错误{exceptionHandlerPathFeature.Error}");
+
+
+            //ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
+            //ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
+            //ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
 
 
             return View("Error");
--- StudentManagement/StudentManagement/Program.cs
+++ StudentManagement/StudentManagement/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace StudentManagement
 {
@@ -18,7 +19,21 @@ namespace StudentManagement
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost.CreateDefaultBuilder(args).ConfigureLogging(
+                (hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                    //启动NLog作为记录日志的程序之一
+                    logging.AddNLog();
+
+
+                }
+                
+                
+                )
             .UseStartup<Startup>();
     }
 }
