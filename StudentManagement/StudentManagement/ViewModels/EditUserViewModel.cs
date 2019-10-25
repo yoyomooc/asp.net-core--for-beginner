802e7252f85d9b48931c6c59769cd62e8a6c0720
桐铭 梁 <werltm@hotmail.com>
2019-10-25 13:49:08 +0800

    完成用户删除功能。

--- StudentManagement/StudentManagement/ViewModels/EditUserViewModel.cs
+++ StudentManagement/StudentManagement/ViewModels/EditUserViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentManagement.CustomerUtil;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,18 +9,20 @@ namespace StudentManagement.ViewModels
 {
     public class EditUserViewModel
     {
-        //public EditUserViewModel()
-        //{
-        //    Claims = new List<string>();
-        //    Roles = new List<string>();
-        //}
+        public EditUserViewModel()
+        {
+            Claims = new List<string>();
+            Roles = new List<string>();
+        }
 
 
         public string Id { get; set; }
         [Required]
         public string UserName { get; set;}
+
         [Required]
         [EmailAddress]
+        [ValidEmailDomain(allowedDomain: "52abp.com", ErrorMessage = "电子邮件后缀必须是52abp.com")]
         public string Email { get; set; }
 
         public  IList<string> Roles{ get; set; }
---
+++ StudentManagement/StudentManagement/wwwroot/js/CustomerScript.js
@@ -0,0 +1,17 @@
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+
+    var deleteSpan = "deleteSpan_" + uniqueId;
+    var confirmDeleteSpan = "confirmDeleteSpan_" + uniqueId;
+
+    if (isDeleteClicked) {
+
+        $("#" + deleteSpan).hide();
+        $("#" + confirmDeleteSpan).show();
+
+    } else {
+        $("#" + deleteSpan).show();
+        $("#" + confirmDeleteSpan).hide();
+    }
+
+
+}
