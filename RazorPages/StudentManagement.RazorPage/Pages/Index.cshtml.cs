9da7278e45a435cb197ccebaa965bb5e02e27a23
桐铭 梁 <werltm@hotmail.com>
2020-09-27 21:39:58 +0800

    调整代码。

--- RazorPages/StudentManagement.RazorPage/Pages/Index.cshtml.cs
+++ RazorPages/StudentManagement.RazorPage/Pages/Index.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,19 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication1.Pages
+namespace StudentManagement.RazorPage.Pages
 {
-    public class PrivacyModel : PageModel
+    public class IndexModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        private readonly ILogger<IndexModel> _logger;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
         public void OnGet()
         {
+
         }
     }
 }
